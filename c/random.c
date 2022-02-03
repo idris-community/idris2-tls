83826933b1b7299c2d18818c69e2ab90e0db9f2d
octeep <github@bandersnatch.anonaddy.com>
2022-02-03 04:16:41 +0000

    simplify windows logic

--- c/random.c
+++ c/random.c
@@ -1,15 +1,13 @@
 #include <stdlib.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+#define __WINDOWS__ 1
+#endif
+
 #ifdef __linux__
 #include <sys/random.h>
-#elif _WIN64
+#elif __WINDOWS__
 #include <windows.h>
-#include <ntstatus.h>
-#include <winerror.h>
-#include <bcrypt.h>
-#elif _WIN32
-#include <windows.h>
-#include <ntstatus.h>
-#include <winerror.h>
 #include <bcrypt.h>
 #endif
 
@@ -17,10 +15,7 @@ int random_buf(void *buf, size_t nbytes) {
 #ifdef __linux__
     ssize_t out = getrandom(buf, nbytes, 0);
     return out < 0;
-#elif _WIN64
-    NTSTATUS status = BCryptGenRandom(NULL, buf, nbytes, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
-    return !BCRYPT_SUCCESS(status);
-#elif _WIN32
+#elif __WINDOWS__
     NTSTATUS status = BCryptGenRandom(NULL, buf, nbytes, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
     return !BCRYPT_SUCCESS(status);
 #else
